f458640666f30809fffa99a999edb611b6019fde
xws <627078086@qq.com>
2017-03-12 16:17:39 +0800

    论文框架

---
+++ AutoSSS/autosss/src/main/java/com/geekxws/autosss/AutosssApplication.java
@@ -0,0 +1,12 @@
+package com.geekxws.autosss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosssApplication.class, args);
+	}
+}
