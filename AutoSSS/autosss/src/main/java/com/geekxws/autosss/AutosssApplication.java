a46cfc2644a58a6b0002f246fd636467af6735fc
xws <627078086@qq.com>
2017-03-12 18:45:09 +0800

    毕设框架，框架勉强运行

--- AutoSSS/autosss/src/main/java/com/geekxws/autosss/AutosssApplication.java
+++ AutoSSS/autosss/src/main/java/com/geekxws/autosss/AutosssApplication.java
@@ -2,10 +2,17 @@ package com.geekxws.autosss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AutosssApplication {
 
+	@RequestMapping("/")
+	public String index() {
+		return "tmd 咩事啊";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AutosssApplication.class, args);
 	}
